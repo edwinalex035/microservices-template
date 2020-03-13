8898ef001ace306ef4acc0ba9af8c1c6a3cbca1e
Edwin Sanchez <edwinalex035@gmail.com>
2020-03-13 09:27:39 -0600

    :tada: Initial commit
    Initial commit

---
+++ src/main/java/com/ed/microservicestemplate/MicroservicesTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.ed.microservicestemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ed/microservicestemplate/MicroservicesTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ed.microservicestemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
